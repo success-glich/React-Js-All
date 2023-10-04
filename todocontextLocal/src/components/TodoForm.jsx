c84ac8c9e5f1af6dcbb688d38f62100f55dd6b85
Ghorasaini Success <imsaphal21@gmail.com>
2023-10-04 14:52:35 +0545

    todo Context With Local Storage

---
+++ todocontextLocal/src/components/TodoForm.jsx
@@ -0,0 +1,31 @@
+import { useState } from "react";
+import { useTodo } from "../context";
+
+function TodoForm() {
+  const { addTodo } = useTodo();
+  const [title, setTitle] = useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    addTodo(title);
+    setTitle("");
+  };
+  return (
+    <form className="flex" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Write Todo..."
+        className="w-full border border-black/10 rounded-l-lg px-3 outline-none duration-150 bg-white/20 py-1.5"
+      />
+      <button
+        type="submit"
+        className="rounded-r-lg px-3 py-1 bg-green-600 text-white shrink-0"
+      >
+        Add
+      </button>
+    </form>
+  );
+}
+
+export default TodoForm;
